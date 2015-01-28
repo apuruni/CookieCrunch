e245c39bb20995068b462285718cf55d6126d4e2
apuruni <apuruni@gmail.com>
2015-01-28 23:27:54 +0900

    finish tutorial 1

--- CookieCrunch/GameScene.swift
+++ CookieCrunch/GameScene.swift
@@ -25,6 +25,11 @@ class GameScene: SKScene {
     
     var swipeHandler: ((Swap) ->())?
     
+    let swapSound = SKAction.playSoundFileNamed("Sounds/Chomp.wav", waitForCompletion: false)
+    let invalidSwapSound = SKAction.playSoundFileNamed("Sounds/Error.wav", waitForCompletion: false)
+    let matchSound = SKAction.playSoundFileNamed("Sounds/Ka-Ching.wav", waitForCompletion: false)
+    let fallingCookieSound = SKAction.playSoundFileNamed("Sounds/Scrape.wav", waitForCompletion: false)
+    let addCookieSound = SKAction.playSoundFileNamed("Sounds/Drip.wav", waitForCompletion: false)
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder) is not used in this app")
@@ -179,6 +184,8 @@ class GameScene: SKScene {
         let moveB = SKAction.moveTo(spriteA.position, duration: duration)
         moveB.timingMode = .EaseOut
         spriteB.runAction(moveB)
+        
+        runAction(swapSound)
     }
     
     func animateInvalidSwap(swap: Swap, completion: () -> () ) {
@@ -199,6 +206,7 @@ class GameScene: SKScene {
         spriteA.runAction(SKAction.sequence([moveA, moveB]), completion: completion)
         spriteB.runAction(SKAction.sequence([moveB, moveA]))
 
+        runAction(invalidSwapSound)
     }
     
     func showSelectionIndicatorForCookie(cookie: Cookie) {
